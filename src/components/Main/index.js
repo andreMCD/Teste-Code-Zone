d3f2c515f25c5fb4ca647e9ee84947f5638cbd8f
andremcd <andredemcd@hotmail.com>
2019-11-10 18:51:16 -0300

    importei os novos components

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import BoxInfoPost from '../BoxInfoPost/index'
 
 import * as S from './styled'
 
 const Main = () => (
     <S.MainWrapper>
-        
+        <BoxInfoPost/>
+        <BoxInfoPost/>
+        <BoxInfoPost/>
+        <BoxInfoPost/>
+        <BoxInfoPost/>
+        <BoxInfoPost/>
     </S.MainWrapper>
 )
 
--- src/components/Main/styled.js
+++ src/components/Main/styled.js
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 
 export const MainWrapper = styled.section`
     grid-area: main;
+    display: grid;
 
-    background: purple;
+
+    background: #404040;
 `
