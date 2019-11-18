b0de1a8843ba0b7f3d969c61e6d551aa2442a50c
andremcd <andredemcd@hotmail.com>
2019-11-18 15:16:34 -0300

    decidi retirar o carrosel

--- src/components/Caroussel/index.js
+++ src/components/Caroussel/index.js
@@ -1,38 +1,12 @@
-import React, { useState, useEffect } from 'react'
-import Carousel from './Carousel'
-import axios from 'axios'
+import React from "react";
 
-import * as S from './styled'
+import * as S from "./styled";
 
-const SPLASHBASE_URL = 'http://www.splashbase.co/api/v1/images/latest';
-  
-
-const Caroussel = (props) => {    
-    const [imgList, setImgList] = useState([]);
-
-    useEffect(() => {
-        axios.get(SPLASHBASE_URL)
-        .then((resp) => {
-            setImgList(resp.data.images);
-        }).catch((err) => {
-      console.log('Unable to Fetch Image from splashbase', err);
-    });
-  }, []);
-
-    return (
-        <S.CarousselWrapper>
-        <S.CarousselContainerImg>
-        {imgList.length === 0 && <div>Loading...</div>}
-      {imgList.length > 0 &&
-        <Carousel 
-            imgList={imgList} 
-            img_width={250} 
-            img_height={200}
-            visibleImages={3} 
-            duration={250}/>
-      }
-        </S.CarousselContainerImg>
+const Caroussel = props => {
+  return (
+    <S.CarousselWrapper>
+      <S.CarousselContainerImg></S.CarousselContainerImg>
     </S.CarousselWrapper>
-    )
-    }
-export default Caroussel
+  );
+};
+export default Caroussel;
--- src/components/Caroussel/styled.js
+++ src/components/Caroussel/styled.js
@@ -1,17 +1,16 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const CarousselWrapper = styled.aside`
-    grid-area: aside;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: aquamarine;
-`
+  grid-area: aside;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #1d2029;
+`;
 export const CarousselContainerImg = styled.div`
-    width: 100%;
-    display: flex;
-    position: relative;
-    left: 0;
-    top: 0;
-
-`
+  width: 100%;
+  display: flex;
+  position: relative;
+  left: 0;
+  top: 0;
+`;
