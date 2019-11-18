9e51dccc6799aeda954b8a79b67336f8eac564ef
andremcd <andredemcd@hotmail.com>
2019-11-18 14:41:28 -0300

    alguns ajustes no style e criei o conteudo do component PostItem

--- src/components/JobsItem/index.js
+++ src/components/JobsItem/index.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
-import * as S from './styled'
+import { Title, Paragraph } from "../Contents/styled";
+
+import * as S from "./styled";
 
 const JobsItem = () => (
-    <S.JobsItemWrapper>
-        
-    </S.JobsItemWrapper>
-)
+  <S.JobsItemWrapper>
+    <Title>Título</Title>
+    <Paragraph>
+      LoremMagna nostrud dolor sunt anim eu duis mollit sint non officia ea.
+      Exercitation non enim do incididunt do ut aute et. Incididunt laborum ex
+      quis eiusmod nulla labore reprehenderit minim labore esse et aute non.
+    </Paragraph>
+  </S.JobsItemWrapper>
+);
 
-export default JobsItem
+export default JobsItem;
--- src/components/JobsItem/styled.js
+++ src/components/JobsItem/styled.js
@@ -1,14 +1,16 @@
-import styled from 'styled-components'
-import media from 'styled-media-query'
+import styled from "styled-components";
+import media from "styled-media-query";
 
 export const JobsItemWrapper = styled.section`
-    width: 100%;
-    height: 300px;
-    display: flex;
+  width: 100%;
+  height: 300px;
+  display: flex;
+  flex-flow: column;
+  padding: 2rem 0;
 
-    background: #fff;
+  background: #fff;
 
-    ${media.lessThan('large')`
+  ${media.lessThan("large")`
         height: 300px;
     `}
-`
+`;
